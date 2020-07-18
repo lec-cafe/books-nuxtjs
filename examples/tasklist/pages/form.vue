56acefa92e18e3a29af12fac20e64b8357ffea02
Yoshi <yoshis1057@gmail.com>
2020-07-18 18:31:15 +0900

    サンプルアプリケーション

---
+++ examples/tasklist/pages/form.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <form>
+      <div class="form-group">
+        <label>タスク</label>
+        <input v-model="task.name" type="text" class="form-control">
+      </div>
+      <div class="form-group">
+        <label>日時</label>
+        <input v-model="task.date" type="date" class="form-control">
+      </div>
+      <button @click="submit" type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        task: {
+          name: "",
+          date: ""
+        },
+      }
+    },
+    methods: {
+      submit() {
+        this.$router.push("/")
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
