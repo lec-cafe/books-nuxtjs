305d31a0e803d062136673370ab38e9903d74919
みかかね <t.goto@chatbox-inc.com>
2020-08-13 19:02:14 +0900

    chatapp

---
+++ docs/lessons/chatapp/sidebar.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "/lessons/chatapp/": [
+        {
+            title: 'Nuxt.js/Firebase で作成するチャットアプリ',
+            collapsable: false,
+            path: "/lessons/chatapp/",
+            children: [
+                '1-1.setup',
+                '1-2.store',
+                '1-3.firebase',
+                '1-4.auth',
+                '1-5.firestore',
+            ]
+        },
+        // {
+        //     title: 'Extra',
+        //     collapsable: false,
+        //     children: [
+        //         '2-2.validation',
+        //         '2-3.vuex',
+        //         '2-4.realtime',
+        //     ]
+        // },
+        "/lessons/",
+        "/tips/nuxtjs/",
+        "/tips/firebase/"
+
+    ],
+}
